e95280e330d25427f84b2dfa6c1d24517167f289
zhenaspaev1974 <zhenaspaev1974@gmail.com>
2026-05-27 09:22:42 +0500

    Update Интересные задачи.cpp

--- Интересные задачи.cpp
+++ Интересные задачи.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,21 +6,41 @@
 using namespace std;
 
 string intToRoman(int num) {
-    // Сопоставление чисел и их римских обозначений (в порядке убывания)
-    vector<pair<int, string>> roman_symbols = {
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-    };
+	// Сопоставление чисел и их римских обозначений (в порядке убывания)
+	vector<pair<int, string>> roman_symbols = {
+		{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+		{100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+		{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+	};
 
-    string result = "";
-    for (const auto& symbol : roman_symbols) {
-        while (num >= symbol.first) {
-            result += symbol.second;
-            num -= symbol.first;
-        }
-    }
-    return result;
+	string result = "";
+	// Проходим по всем парам (число - символ)
+	for (const auto& symbol : roman_symbols) {
+		// Пока текущее число больше или равно значению символа
+		while (num >= symbol.first) {
+			result += symbol.second; // Добавляем символ в ответ
+			num -= symbol.first;     // Вычитаем значение из числа
+		}
+	}
+	return result;
 }
 
-// Пример вызова: intToRoman(1994) вернет "MCMXCIV"
+int main() {
+	// Пример из задания
+	int number1 = 42;
+	cout << "Number: " << number1 << " -> Roman: " << intToRoman(number1) << "\n";
+
+	// Пример из текста задания про 1994 год
+	int number2 = 1994;
+	cout << "Number: " << number2 << " -> Roman: " << intToRoman(number2) << "\n";
+
+	// Можно добавить ввод с клавиатуры
+	int user_num;
+	cout << "\nEnter your number (from 1 to 3999): ";
+	cin >> user_num;
+	if (user_num > 0) {
+		cout << "Result: " << intToRoman(user_num) << "\n";
+	}
+
+	return 0;
+}
